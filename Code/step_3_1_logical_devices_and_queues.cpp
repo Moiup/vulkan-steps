469af97d7df6d668df1d05474952ece2711198b4
moiup <mohammedbashir.m@gmail.com>
2022-09-20 19:04:22 +0200

    Logical devices and queues.

---
+++ Code/step_3_1_logical_devices_and_queues.cpp
@@ -0,0 +1,299 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <vector>
+#include <string>
+#include <vulkan/vulkan.h>
+
+int main()
+{
+	/**************************************************************/
+	/* Step 0: Window creation with GLFW                          */
+	/**************************************************************/
+	VkResult vk_result = VK_SUCCESS;
+
+	const uint64_t width{ 1280 };
+	const uint64_t height{ 720 };
+
+	GLFWwindow* window;
+
+	std::cout << "Initializing GLFW..." << std::endl;
+	if (!glfwInit()) {
+		throw std::runtime_error("Failed to initialise GLFW");
+	}
+	std::cout << "GLFW initialized with success." << std::endl;
+
+	std::cout << "GLFW window creation." << std::endl;
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	window = glfwCreateWindow(width, height, "Le Cube", nullptr, nullptr);
+	if (!window) {
+		glfwTerminate();
+		throw std::runtime_error("Window creation failed.");
+	}
+	std::cout << "GLFW window created successfully." << std::endl;
+
+	/**************************************************************/
+	/* Step 1: Instances (4.2.)                                   */
+	/**************************************************************/
+	VkInstance vk_instance{};
+	VkApplicationInfo vk_application_info{};
+	vk_application_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	vk_application_info.pNext = nullptr;
+	vk_application_info.pApplicationName = "hello world cube";
+	vk_application_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	vk_application_info.pEngineName = "no engine";
+	vk_application_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	vk_application_info.apiVersion = VK_API_VERSION_1_3;
+
+	uint32_t glfw_extension_count = 0;
+	const char** glfw_extensions;
+	glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+	uint32_t instance_layer_prop_count = 0;
+	std::vector<VkLayerProperties> layer_properties;
+	vkEnumerateInstanceLayerProperties(&instance_layer_prop_count, nullptr);
+	layer_properties.resize(instance_layer_prop_count);
+	vkEnumerateInstanceLayerProperties(&instance_layer_prop_count, layer_properties.data());
+	std::cout << "Available layers:" << std::endl;
+	bool is_validation_layer = false;
+	for (auto layer_prop : layer_properties) {
+		std::string layer_name = std::string(layer_prop.layerName);
+		std::cout << "\t" << layer_name << std::endl;
+		is_validation_layer = is_validation_layer || (layer_name == "VK_LAYER_KHRONOS_validation");
+	}
+	if (!is_validation_layer) {
+		throw std::runtime_error("Validation layer not found.");
+	}
+
+	std::vector<const char*> enabled_layers = {
+		"VK_LAYER_KHRONOS_validation"
+	};
+
+	VkInstanceCreateInfo vk_instance_create_info{};
+	vk_instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	vk_instance_create_info.pNext = nullptr;
+	vk_instance_create_info.flags = 0;
+	vk_instance_create_info.pApplicationInfo = &vk_application_info;
+	vk_instance_create_info.enabledLayerCount = enabled_layers.size();
+	vk_instance_create_info.ppEnabledLayerNames = enabled_layers.data();
+	vk_instance_create_info.enabledExtensionCount = glfw_extension_count;
+	vk_instance_create_info.ppEnabledExtensionNames = glfw_extensions;
+
+	vk_result = vkCreateInstance(&vk_instance_create_info, nullptr, &vk_instance);
+	if (vk_result != VK_SUCCESS) {
+		throw std::runtime_error("Instance creation failed!");
+	}
+	std::cout << "Instance created successfully." << std::endl;
+
+	/**************************************************************/
+	/* Step 2: Surface                                            */
+	/**************************************************************/
+	VkSurfaceKHR surface;
+	vk_result = glfwCreateWindowSurface(
+		vk_instance,
+		window,
+		nullptr,
+		&surface
+	);
+	if (vk_result != VK_SUCCESS) {
+		throw std::runtime_error("Error: failed to create window surface.");
+	}
+
+	/**************************************************************/
+	/* Step 3: Devices and Queues (5.)                            */
+	/**************************************************************/
+	/**************************************************************/
+	/* Step 3.1: Physical Devices (5.1)                           */
+	/**************************************************************/
+	std::vector<VkPhysicalDevice> physical_devices;
+	uint32_t physical_device_count = 0;
+
+	// Get the count
+	vkEnumeratePhysicalDevices(vk_instance, &physical_device_count, nullptr);
+	vk_result = vkEnumeratePhysicalDevices(vk_instance, &physical_device_count, physical_devices.data());
+	if (vk_result != VK_SUCCESS) {
+		throw std::runtime_error("Unable to count physical devices.");
+	}
+	// Get the actual array of devices
+	physical_devices.resize(physical_device_count);
+	vk_result = vkEnumeratePhysicalDevices(vk_instance, &physical_device_count, physical_devices.data());
+	if (vk_result != VK_SUCCESS) {
+		throw std::runtime_error("Unable to enumerate physical devices.");
+	}
+
+	std::cout << physical_devices.size() << std::endl;
+	VkPhysicalDevice discrete_gpu{};
+	VkPhysicalDeviceProperties discrete_gpu_properties;
+	for (auto physical_device_tmp : physical_devices) {
+		VkPhysicalDeviceProperties properties{};
+		vkGetPhysicalDeviceProperties(physical_device_tmp, &properties);
+		std::cout << "Device: " << std::endl;
+		std::cout << "\t";
+		std::cout << properties.deviceName << std::endl;
+		if (properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+			discrete_gpu = physical_device_tmp;
+			discrete_gpu_properties = properties;
+		}
+	}
+	std::cout << "Selected physical device: " << std::endl;
+	std::cout << "\t" << discrete_gpu_properties.deviceName << std::endl;
+
+	/**************************************************************/
+	/* Step 3.2: Logical Devices and Queues (5.2 and 5.3)         */
+	/**************************************************************/
+	// Retreiving existing queue
+	uint32_t queue_family_property_count;
+	std::vector<VkQueueFamilyProperties> queue_family_propertie_arr;
+	vkGetPhysicalDeviceQueueFamilyProperties(
+		discrete_gpu,
+		&queue_family_property_count,
+		nullptr
+	);
+	queue_family_propertie_arr.resize(queue_family_property_count);
+	vkGetPhysicalDeviceQueueFamilyProperties(
+		discrete_gpu,
+		&queue_family_property_count,
+		queue_family_propertie_arr.data()
+	);
+	if (queue_family_propertie_arr.size() == 0) {
+		throw std::runtime_error("Error: no queue family found!");
+	}
+
+	// For now, we want one queue family composed of one queue
+	// GRAPHICS QUEUE
+	VkQueue graphic_queue = nullptr;
+	int32_t nb_graphics_queue_families_to_create = 1;
+	std::vector<VkDeviceQueueCreateInfo> graphics_queues_arr(nb_graphics_queue_families_to_create);
+	for (uint32_t i = 0; i < graphics_queues_arr.size(); i++) {
+		if (!(queue_family_propertie_arr[i].queueFlags & VK_QUEUE_GRAPHICS_BIT
+			&&
+			queue_family_propertie_arr[i].queueFlags & VK_QUEUE_COMPUTE_BIT
+			)) {
+			continue;
+		}
+
+		std::vector<float> queue_priorities(1, 1.0f);
+
+		VkDeviceQueueCreateInfo queue{};
+		queue.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queue.pNext = nullptr;
+		queue.queueFamilyIndex = i;
+		queue.queueCount = 1;
+		queue.pQueuePriorities = queue_priorities.data();
+
+		graphics_queues_arr[i] = queue;
+
+		nb_graphics_queue_families_to_create--;
+		if (nb_graphics_queue_families_to_create == 0) {
+			break;
+		}
+	}
+	if (nb_graphics_queue_families_to_create > 0) {
+		throw std::runtime_error("Error: the right amount of queue was not found!");
+	}
+
+	// PRESENTATION QUEUE
+	VkQueue present_queue = nullptr;
+	int32_t nb_present_queue_families_to_create = 1;
+	std::vector<VkDeviceQueueCreateInfo> present_queues_arr(nb_present_queue_families_to_create);
+	for (uint32_t i = 0; i < present_queues_arr.size(); i++) {
+		if (!(queue_family_propertie_arr[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)) {
+			continue;
+		}
+		VkBool32 supported;
+		vkGetPhysicalDeviceSurfaceSupportKHR(
+			discrete_gpu,
+			i,
+			surface,
+			&supported
+		);
+		if (supported == VK_FALSE) {
+			continue;
+		}
+
+		std::vector<float> queue_priorities(1, 1.0f);
+
+		VkDeviceQueueCreateInfo queue{};
+		queue.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queue.pNext = nullptr;
+		queue.queueFamilyIndex = i;
+		queue.queueCount = 1;
+		queue.pQueuePriorities = queue_priorities.data();
+
+		present_queues_arr[i] = queue;
+
+		nb_present_queue_families_to_create--;
+		if (nb_present_queue_families_to_create == 0) {
+			break;
+		}
+	}
+	if (nb_present_queue_families_to_create > 0) {
+		throw std::runtime_error("Error: the right amount of queue was not found!");
+	}
+
+	std::vector<VkDeviceQueueCreateInfo> queues_create_info{ graphics_queues_arr[0] };
+	std::vector<uint32_t> queue_family_indexes = { graphics_queues_arr[0].queueFamilyIndex };
+
+	// Checking if the found queue are in fact from the same family or not
+	if (present_queues_arr[0].queueFamilyIndex != graphics_queues_arr[0].queueFamilyIndex) {
+		queues_create_info.push_back(present_queues_arr[0]);
+		queue_family_indexes.push_back(present_queues_arr[0].queueFamilyIndex);
+	}
+
+	// Creating the LOGICAL DEVICE
+	const std::vector<const char*> device_extensions = {
+		VK_KHR_SWAPCHAIN_EXTENSION_NAME
+	};
+
+	VkDevice logical_device;
+	VkPhysicalDeviceFeatures device_features{};
+	VkDeviceCreateInfo device_create_info{};
+	device_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	device_create_info.pNext = nullptr;
+	//device_create_info.flags;
+	device_create_info.queueCreateInfoCount = queues_create_info.size();
+	device_create_info.pQueueCreateInfos = queues_create_info.data();
+	device_create_info.enabledExtensionCount = device_extensions.size();
+	device_create_info.ppEnabledExtensionNames = device_extensions.data();
+	device_create_info.pEnabledFeatures = &device_features;
+
+	vk_result;
+	vk_result = vkCreateDevice(
+		discrete_gpu,
+		&device_create_info,
+		nullptr,
+		&logical_device
+	);
+	if (vk_result != VK_SUCCESS) {
+		throw std::runtime_error("Error creating logical device!");
+	}
+	else {
+		std::cout << "Logical device created." << std::endl;
+	}
+
+	vkGetDeviceQueue(
+		logical_device,
+		graphics_queues_arr[0].queueFamilyIndex,
+		0,
+		&graphic_queue
+	);
+	vkGetDeviceQueue(
+		logical_device,
+		present_queues_arr[0].queueFamilyIndex,
+		0,
+		&present_queue
+	);
+
+	/********************/
+	/* Game Loop        */
+	/********************/
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+}
--- Tutorial/logical_devices_and_queues.md
+++ Tutorial/logical_devices_and_queues.md
@@ -1,15 +1,168 @@
 # **Step 3.2: Logical Devices and Queues (5.2. and 5.3.)**
 ## **Hints**
+The goal is to create a logical device by calling the function [`vkCreateDevice`](https://registry.khronos.org/vulkan/specs/1.3-extensions/html/chap5.html#vkCreateDevice). From there, the structure [`VkDeviceCreateInfo`](https://registry.khronos.org/vulkan/specs/1.3-extensions/html/chap5.html#VkDeviceCreateInfo) will show us that it needs queues.
 
 ## **The Code**
+First thing first, we need to retreive the existing queue family properties by calling [`vkGetPhysicalDeviceQueueFamilyProperties`](https://registry.khronos.org/vulkan/specs/1.3-extensions/html/chap5.html#VkDeviceCreateInfo). 
 
+We will need two queues. For each of them, we need to iterate throug the properties found previously, and look for a family containing the the wanted flags in the `queueFlags` field of the [`VkQueueFamilyProperties`](https://registry.khronos.org/vulkan/specs/1.3-extensions/html/chap5.html#VkQueueFamilyProperties) structure.
+
+Next, we need to define an array of extensions, here it contains only the value [`VK_KHR_SWAPCHAIN_EXTENSION_NAME`](https://registry.khronos.org/vulkan/specs/1.3-extensions/html/chap50.html#_new_enum_constants_37).
+
+With the [`VkDeviceQueueCreateInfo`](https://registry.khronos.org/vulkan/specs/1.3-extensions/html/chap5.html#VkDeviceQueueCreateInfo) filled previously and the extensions' arrray, one can finaly fill the [`VkDeviceCreateInfo`](https://registry.khronos.org/vulkan/specs/1.3-extensions/html/chap5.html#VkDeviceCreateInfo) structure and then call [`vkCreateDevice`](https://registry.khronos.org/vulkan/specs/1.3-extensions/html/chap5.html#vkCreateDevice).
+
+After creating the logical device, we need to get the actual queues with [`vkGetDeviceQueue`](https://registry.khronos.org/vulkan/specs/1.3-extensions/html/chap5.html#vkGetDeviceQueue).
 
 ```C++
-    
+    /**************************************************************/
+	/* Step 3.2: Logical Devices and Queues (5.2 and 5.3)         */
+	/**************************************************************/
+	// Retreiving existing queue
+	uint32_t queue_family_property_count;
+	std::vector<VkQueueFamilyProperties> queue_family_propertie_arr;
+	vkGetPhysicalDeviceQueueFamilyProperties(
+		discrete_gpu,
+		&queue_family_property_count,
+		nullptr
+	);
+	queue_family_propertie_arr.resize(queue_family_property_count);
+	vkGetPhysicalDeviceQueueFamilyProperties(
+		discrete_gpu,
+		&queue_family_property_count,
+		queue_family_propertie_arr.data()
+	);
+	if (queue_family_propertie_arr.size() == 0) {
+		throw std::runtime_error("Error: no queue family found!");
+	}
+
+	// For now, we want one queue family composed of one queue
+	// GRAPHICS QUEUE
+	VkQueue graphic_queue = nullptr;
+	int32_t nb_graphics_queue_families_to_create = 1;
+	std::vector<VkDeviceQueueCreateInfo> graphics_queues_arr(nb_graphics_queue_families_to_create);
+	for (uint32_t i = 0; i < graphics_queues_arr.size(); i++) {
+		if (!(queue_family_propertie_arr[i].queueFlags & VK_QUEUE_GRAPHICS_BIT
+			&&
+			queue_family_propertie_arr[i].queueFlags & VK_QUEUE_COMPUTE_BIT
+			)) {
+			continue;
+		}
+
+		std::vector<float> queue_priorities(1, 1.0f);
+
+		VkDeviceQueueCreateInfo queue{};
+		queue.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queue.pNext = nullptr;
+		queue.queueFamilyIndex = i;
+		queue.queueCount = 1;
+		queue.pQueuePriorities = queue_priorities.data();
+
+		graphics_queues_arr[i] = queue;
+
+		nb_graphics_queue_families_to_create--;
+		if (nb_graphics_queue_families_to_create == 0) {
+			break;
+		}
+	}
+	if (nb_graphics_queue_families_to_create > 0) {
+		throw std::runtime_error("Error: the right amount of queue was not found!");
+	}
+
+	// PRESENTATION QUEUE
+	VkQueue present_queue = nullptr;
+	int32_t nb_present_queue_families_to_create = 1;
+	std::vector<VkDeviceQueueCreateInfo> present_queues_arr(nb_present_queue_families_to_create);
+	for (uint32_t i = 0; i < present_queues_arr.size(); i++) {
+		if (!(queue_family_propertie_arr[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)) {
+			continue;
+		}
+		VkBool32 supported;
+		vkGetPhysicalDeviceSurfaceSupportKHR(
+			discrete_gpu,
+			i,
+			surface,
+			&supported
+		);
+		if (supported == VK_FALSE) {
+			continue;
+		}
+
+		std::vector<float> queue_priorities(1, 1.0f);
+
+		VkDeviceQueueCreateInfo queue{};
+		queue.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queue.pNext = nullptr;
+		queue.queueFamilyIndex = i;
+		queue.queueCount = 1;
+		queue.pQueuePriorities = queue_priorities.data();
+
+		present_queues_arr[i] = queue;
+
+		nb_present_queue_families_to_create--;
+		if (nb_present_queue_families_to_create == 0) {
+			break;
+		}
+	}
+	if (nb_present_queue_families_to_create > 0) {
+		throw std::runtime_error("Error: the right amount of queue was not found!");
+	}
+
+	std::vector<VkDeviceQueueCreateInfo> queues_create_info{ graphics_queues_arr[0] };
+	std::vector<uint32_t> queue_family_indexes = { graphics_queues_arr[0].queueFamilyIndex };
+
+    // Checking if the found queue are in fact from the same family or not
+	if (present_queues_arr[0].queueFamilyIndex != graphics_queues_arr[0].queueFamilyIndex) {
+		queues_create_info.push_back(present_queues_arr[0]);
+		queue_family_indexes.push_back(present_queues_arr[0].queueFamilyIndex);
+	}
+
+	// Creating the LOGICAL DEVICE
+	const std::vector<const char*> device_extensions = {
+		VK_KHR_SWAPCHAIN_EXTENSION_NAME
+	};
+
+	VkDevice logical_device;
+	VkPhysicalDeviceFeatures device_features{};
+	VkDeviceCreateInfo device_create_info{};
+	device_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	device_create_info.pNext = nullptr;
+	//device_create_info.flags;
+	device_create_info.queueCreateInfoCount = queues_create_info.size();
+	device_create_info.pQueueCreateInfos = queues_create_info.data();
+	device_create_info.enabledExtensionCount = device_extensions.size();
+	device_create_info.ppEnabledExtensionNames = device_extensions.data();
+	device_create_info.pEnabledFeatures = &device_features;
+
+	vk_result;
+	vk_result = vkCreateDevice(
+		discrete_gpu,
+		&device_create_info,
+		nullptr,
+		&logical_device
+	);
+	if (vk_result != VK_SUCCESS) {
+		throw std::runtime_error("Error creating logical device!");
+	}
+	else {
+		std::cout << "Logical device created." << std::endl;
+	}
+
+	vkGetDeviceQueue(
+		logical_device,
+		graphics_queues_arr[0].queueFamilyIndex,
+		0,
+		&graphic_queue
+	);
+	vkGetDeviceQueue(
+		logical_device,
+		present_queues_arr[0]. ,
+		0,
+		&present_queue
+	);
 ```
 
-File: [](../Code/)
+File: [step_3_1_logical_devices_and_queues.cpp](../Code/step_3_1_logical_devices_and_queues.cpp)
 
 | Previous | Next |
 |---|---|
-| []() | []() |
+| [Step 3.1: Physical Devices](physical_devices.md) | [Step 3.2: Logical Devices and Queues](logical_devices_and_queues.md) |
